eb3d821252e41d752e870cb1476fbc243921e808
yakub.pujangga <yakub.jobs@gmail.com>
2017-01-09 07:50:00 +0700

    fixing category and category detail url and banner

--- src/main/java/com/special/gift/app/controller/ui/UiController.java
+++ src/main/java/com/special/gift/app/controller/ui/UiController.java
@@ -29,6 +29,9 @@ public class UiController {
   public static final String HELP = PATH + "help";
   public static final String SEARCH = PATH + "search";
   public static final String NOTIFICATION = PATH + "notification";
+
+  // used for category > child > grandchild
+  public static final String CATEGORIES = PATH + "categories";
   public static final String DETAIL = PATH + "categories/{criteria}";
   public static final String DETAIL_CHILD = PATH + "categories/{criteria}/{child}";
 
@@ -56,28 +59,46 @@ public class UiController {
     return "/contents/search";
   }
 
+  @RequestMapping(value = CATEGORIES, method = RequestMethod.GET)
+  public String renderCategoriesPage(Model model, HttpSession session) {
+    model.addAttribute("user", session.getAttribute("user"));
+    return "/contents/parent-categories";
+  }
+
   @RequestMapping(value = DETAIL, method = RequestMethod.GET)
   public String renderDetailPage(Model model, @PathVariable(value = "criteria") String criteria,
       @RequestParam(value = "c") String id, HttpSession session) {
     model.addAttribute("user", session.getAttribute("user"));
-    model.addAttribute("parent", criteria.replace("-", " "));
 
-    final VendorDesc vendorDesc = vendorDescService.findById(id);
-
-    if (id == null || criteria == null || vendorDesc == null) {
+    if (id == null || criteria == null || id == null) {
       return "redirect:/";
+    } else {
+      // used for url
+      model.addAttribute("parent", criteria.replace(" ", "-"));
+
+      // used for banner
+      model.addAttribute("category_name", criteria.replace("-", " "));
+
+      log.debug("parent_from detail : {}", criteria);
+
+      final VendorDesc vendorDesc = vendorDescService.findById(id);
+
+      if (vendorDesc == null) {
+        return "redirect:/";
+      } else {
+
+        model.addAttribute("children1",
+            vendorDescService.findAllChildren(vendorDesc.getVendorType(), 0, 6));
+        model.addAttribute("children2",
+            vendorDescService.findAllChildren(vendorDesc.getVendorType(), 6, 13));
+        model.addAttribute("children3",
+            vendorDescService.findAllChildren(vendorDesc.getVendorType(), 13, 19));
+
+        log.debug("vendor desc : {}", vendorDesc.toString());
+
+        return "/contents/finder";
+      }
     }
-
-    model.addAttribute("children1",
-        vendorDescService.findAllChildren(vendorDesc.getVendorType(), 0, 6));
-    model.addAttribute("children2",
-        vendorDescService.findAllChildren(vendorDesc.getVendorType(), 6, 13));
-    model.addAttribute("children3",
-        vendorDescService.findAllChildren(vendorDesc.getVendorType(), 13, 19));
-
-    log.debug("vendor desc : {}", vendorDesc.toString());
-
-    return "/contents/finder";
   }
 
 
@@ -86,30 +107,53 @@ public class UiController {
       @PathVariable(value = "criteria") String criteria,
       @PathVariable(value = "child") String child, @RequestParam(value = "c") String id,
       HttpSession session) {
-    final VendorDesc parent = vendorDescService.findById(id.substring(0, 1).concat("0"));
 
-    model.addAttribute("user", session.getAttribute("user"));
-    model.addAttribute("root", criteria.replace("-", " "));
-    model.addAttribute("root_type", parent.getVendorType());
-    model.addAttribute("parent", child.replace("-", " "));
-    model.addAttribute("parent_variable", parent.getVendorDescription().replace(" ", "-"));
-
-    final VendorDesc vendorDesc = vendorDescService.findById(id);
-
-    if (id == null || criteria == null || vendorDesc == null) {
+    if (id == null || criteria == null || id == null) {
       return "redirect:/";
+    } else {
+
+      final VendorDesc parent = vendorDescService.findById(id.substring(0, 1).concat("0"));
+
+      if (parent == null) {
+        return "redirect:/";
+      } else {
+
+        model.addAttribute("user", session.getAttribute("user"));
+
+        // for request param
+        model.addAttribute("root_type", parent.getVendorType());
+
+        // child name banner
+        log.debug("child name : {}", child.replace("-", " "));
+        model.addAttribute("child_name", child.replace("-", " "));
+
+        // parent for parent url
+        model.addAttribute("parent", parent.getVendorDescription().replace(" ", "-").toLowerCase());
+
+        // parent for parent banner
+        model.addAttribute("parentName", parent.getVendorDescription());
+
+
+        final VendorDesc vendorDesc = vendorDescService.findById(id);
+
+        if (vendorDesc == null) {
+          return "redirect:/".concat("categories/")
+              .concat(parent.getVendorDescription().replace(" ", "-").toLowerCase()).concat("?")
+              .concat("c=").concat(parent.getVendorType());
+        }
+
+        model.addAttribute("children1",
+            vendorDescService.findAllChildren(vendorDesc.getVendorType(), 0, 6));
+        model.addAttribute("children2",
+            vendorDescService.findAllChildren(vendorDesc.getVendorType(), 6, 13));
+        model.addAttribute("children3",
+            vendorDescService.findAllChildren(vendorDesc.getVendorType(), 13, 19));
+
+        log.debug("vendor desc : {}", vendorDesc.toString());
+
+        return "/contents/finder";
+      }
     }
-
-    model.addAttribute("children1",
-        vendorDescService.findAllChildren(vendorDesc.getVendorType(), 0, 6));
-    model.addAttribute("children2",
-        vendorDescService.findAllChildren(vendorDesc.getVendorType(), 6, 13));
-    model.addAttribute("children3",
-        vendorDescService.findAllChildren(vendorDesc.getVendorType(), 13, 19));
-
-    log.debug("vendor desc : {}", vendorDesc.toString());
-
-    return "/contents/finder";
   }
 
   @RequestMapping(value = HELP, method = RequestMethod.GET)
